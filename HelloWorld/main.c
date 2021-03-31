c5bc323d28763cce9060e2dc3ac072584b2f8b48
bgvmysore <bgvmysore@gmail.com>
2021-03-31 14:35:27 +0530

    other dummy projects

---
+++ HelloWorld/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char const* mssg = "Gg World!\n";
+    printf("%s", mssg);
+    getchar();
+    return 0;
+}
+
---
+++ sqrt_ex/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	printf("Square root of 2 is %f\n", sqrt(2));
+	return 0;
+}
